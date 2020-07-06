ebff4303626baadda0a9363d595fcbd42a64b435
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-07-06 12:45:29 -0400

    fixed / get route

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,9 +2,9 @@ const path = require("path");
 const router = require('express').Router()
 
 
-// router.get("/", function (req, res) {
-//     res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 router.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
